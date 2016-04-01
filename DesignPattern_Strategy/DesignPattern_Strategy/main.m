dd9124253f91eab80592c35fe526e3a4e6880cbb
enteris <enteris@12corp.com>
2016-04-01 14:57:41 +0900

    전략패턴 추가

--- DesignPattern_Strategy/DesignPattern_Strategy/main.m
+++ DesignPattern_Strategy/DesignPattern_Strategy/main.m
@@ -12,8 +12,8 @@
 #import "Swrod.h"
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        //전략패턴
         //캐릭터는 무기를 가지고 있고 무기를 여러가지로 변경할 수 있다.
-        
         Character *unit = [[Character alloc] init];
         unit.weapon = [[Knife alloc] init];
         [unit attack];
